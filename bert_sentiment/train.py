f493968b70f5bf6e3b31a037f7929adc048fb465
abdelrahman2018-cloud <57602360+abdelrahman2018-cloud@users.noreply.github.com>
2022-04-21 20:44:14 +0200

    Update train.py

--- bert_sentiment/train.py
+++ bert_sentiment/train.py
@@ -57,7 +57,7 @@ def train(
     binary=False,
     bert="bert-base-uncased",
     epochs=10,
-    batch_size=1,
+    batch_size=16,
     save=False,
 ):
     trainset = SSTDataset("train", root=root, binary=binary)
